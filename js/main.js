27249cbce6c75e1da0a75afa21928205127f4d39
Mina-Emad-20 <167125155+Mina-Emad-20@users.noreply.github.com>
2024-08-19 22:23:12 +0300

    burger-web

---
+++ js/main.js
@@ -0,0 +1,74 @@
+//! show menu
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
+
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+//! remove menu mobile
+const navLink = document.querySelectorAll('.nav__link')
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    //! when we click in each navLink we remove the showMenu
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+//! change background header
+const shadowHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('shadow-header') : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
+
+//! show scroll up
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll', scrollUp)
+
+//! scroll sections active link
+const sections = document.querySelectorAll('sectiom[id')
+const scrollActive = () => {
+    const scrollDown = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionClass = document.querySelector('nav__menu a[href*=' + sectionId + ']')
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionClass.classList.add('active-link')
+        } else {
+            sectionClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+//! scroll reveal animation
+var sr = scrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 300,
+    reset: true //animation repeat 
+});
+sr.reveal(`.home__data`)
+sr.reveal(`.home__dish`, { delay: 500, distance: '100px', origin: 'bottom' })
+sr.reveal(`.home__burger`, { delay: 1200, distance: '100px', duration: 1500 })
+sr.reveal(`.home__ingredient`, { delay: 1600, interval: 100 })
+sr.reveal(`.recipe__img, .delivery__img, .contact__data`, { origin: 'left' })
+sr.reveal(`.recipe__data, .delivery__data, .contact__image`, { origin: 'right' })
+sr.reveal(`.popular__card`, { interval: 100 })
